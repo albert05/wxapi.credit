bbb80daade2a40a9240fffa8717a15724fe94749
chenwenfeng <chenwenfeng@wzdai.com>
2018-11-22 18:36:00 +0800

    restart sh

--- restart.sh
+++ restart.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
+git checkout .
 git pull origin master
 #tar -xzvf beepkg.tar.gz
 rm -rf conf/app.conf
 cp conf/app.conf.bat conf/app.conf
-supervisorctl restart wxapi
+#supervisorctl restart wxapi
