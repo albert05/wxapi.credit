5111c59ca8e88572495556e4471c8062116ab924
albert <fengelom@qq.com>
2018-12-02 11:02:33 +0800

    add reload

---
+++ reload.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+# update code
+git checkout .
+git pull origin master
+
+# copy config
+rm -rf conf/app.conf
+cp conf/app.conf.bat conf/app.conf
+
+# stop fs
+supervisorctl stop wxapi
+
+# get listen port
+pid = `lsof -i:8888 | grep LISTEN | awk -F '[ ]+' '{print $2}'`
+
+# kill port listen
+kill ${pid}
+
+# start fs
+supervisorctl stop wxapi
+
+# re grant authorization
+chmod -R 755 restart.sh
--- restart.sh
+++ restart.sh
@@ -1,9 +1,8 @@
 #! /bin/bash
 git checkout .
 git pull origin master
-#tar -xzvf beepkg.tar.gz
+
 rm -rf conf/app.conf
 cp conf/app.conf.bat conf/app.conf
-#supervisorctl restart wxapi
 
 chmod -R 755 restart.sh
